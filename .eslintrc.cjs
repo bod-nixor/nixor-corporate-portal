81e6f1b43046a448756b5ab65c5f1f374594d4a2
Board of Directors <b.o.d@nixorcollege.edu.pk>
2025-11-10 04:33:04 +0100

    chore(eslint): add TypeScript plugin and Node/ES2021 env to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,21 @@
+// .eslintrc.cjs
 module.exports = {
   root: true,
-  extends: ["next/core-web-vitals", "eslint:recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  env: {
+    es2021: true,
+    node: true,
+    browser: true
+  },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
-    "no-unused-vars": ["error", { args: "none" }]
+    "no-unused-vars": ["error", { args: "none" }],
+    "@typescript-eslint/no-var-requires": "off"
   }
 };
